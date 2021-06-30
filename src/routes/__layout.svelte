512f80cb69ba6cafc89a6c97b37a0d8fd963219e
Marcin <jerzakm@gmail.com>
2021-06-30 22:08:27 +0200

    added firebaseUI kit in /login route

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,11 +3,18 @@
 	import { page } from '$app/stores';
 
 	import { authStore } from '$lib/firebase/core';
+	import { onMount } from 'svelte';
+
+	let mounted = false;
+
+	onMount(() => (mounted = true));
 
 	authStore.subscribe((u) => {
 		// redirects for auth status changes
-		if (!u) goto('/login');
-		if (u && $page.path == '/login') goto('/');
+		if (mounted) {
+			if (!u) goto('/login');
+			if (u && $page.path == '/login') goto('/');
+		}
 	});
 </script>
 
--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
-	import { signInWithGoogle } from '$lib/firebase/core';
+	import * as firebaseui from 'firebaseui';
+	import 'firebaseui/dist/firebaseui.css';
+	import { auth, signInWithGoogle } from '$lib/firebase/core';
+	import { onMount } from 'svelte';
+	import firebase from 'firebase';
+
+	onMount(() => {
+		if (firebaseui.auth.AuthUI.getInstance()) {
+			const ui = firebaseui.auth.AuthUI.getInstance();
+			ui.start('#firebaseui-auth-container', {
+				signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
+			});
+		} else {
+			const ui = new firebaseui.auth.AuthUI(firebase.auth());
+			ui.start('#firebaseui-auth-container', {
+				signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
+			});
+		}
+	});
 </script>
 
-<button on:click={() => signInWithGoogle()}>G sign in</button>
+<div id="firebaseui-auth-container" />
